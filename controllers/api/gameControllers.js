3e47425ad4b4b9c444f32664d58c42f70d19b352
dcrlsn <c4r150n@gmail.com>
2022-01-10 10:41:36 -0500

    logins, may be in wrong spot

--- controllers/api/gameControllers.js
+++ controllers/api/gameControllers.js
@@ -12,8 +12,7 @@ const getAllGames = async (req, res) => {
       'Authorization': 'Bearer ' + process.env.AUTH,
     },
     data: `search "forza"; fields name,cover.image_id,platforms.name,release_dates.date,game_modes.name,summary;
-    limit 5;
-    sort release_dates.date desc;`
+    limit 5;`
   })
 
   return res.json(response.data)
@@ -32,8 +31,19 @@ const getGameById = async (req, res) => {
 
   return res.json(response.data)
 }
-const createGame = async () => {
 
+const createGame = async (req, res) => {
+  try {
+    const dbGameData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.status(200).json(dbGameData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 }
 const updateGame = async () => {
 
