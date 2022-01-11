3edc478dfbb77f8450f40f57bfd9dfa3d9728714
dcrlsn <c4r150n@gmail.com>
2022-01-11 15:19:05 -0500

    minor changes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,34 +2,7 @@ const router = require('express').Router();
 const { User, Review, Game } = require('../models');
 const withAuth = require('../utils/auth');
 const axios = require('axios')
-const { apiRequestForGames } = require('../controllers/api/gameControllers.js')
-
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
-
-//     const users = userData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get('/login', (req, res) => {
-//   if (req.session.logged_in) {
-//     res.redirect('/');
-//     return;
-//   }
-
-//   res.render('login');
-// });
+const { apiRequestForGames, getGameById } = require('../controllers/api/gameControllers.js')
 
 router.get('/', async (req, res) => {
   try {
