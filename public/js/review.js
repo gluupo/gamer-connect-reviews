d316e9fedf46b8a577509e3e43cb8363ba92382f
dcrlsn <c4r150n@gmail.com>
2022-01-11 21:01:08 -0500

    review front end

--- public/js/review.js
+++ public/js/review.js
@@ -1,1 +1,24 @@
-const rating = document.querySelector('input[name="rate"]:checked').value
+const reviewFormHandler = async (event) => {
+  event.preventDefault();
+
+  const rating = document.querySelector('input[name="rate"]:checked').value
+  const reviewText = document.querySelector('#review-text').value
+
+  if (rating && reviewText) {
+    const response = await fetch('/api/reviews', {
+      method: 'POST',
+      body: JSON.stringify({ rating, reviewText }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log(response)
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('#review')
+  .addEventListener('submit', reviewFormHandler);
